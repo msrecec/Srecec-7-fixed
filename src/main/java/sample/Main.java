0eb2ea74fb7d9cddcae6c8c6b6d8d235d2dd4596
Mislav Srecec <mislav.srecec3@gmail.com>
2020-12-10 06:07:30 +0100

    Finished

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -39,7 +39,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("pocetniEkran.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Početni ekran");
         primaryStage.setScene(new Scene(root, 550, 380));
         mainStage = primaryStage;
         primaryStage.show();
